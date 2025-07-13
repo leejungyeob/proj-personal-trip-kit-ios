16cdf44c84534ff174bdfaf61d8a14ae4d07f875
DONOTINTO <goods99j@gmail.com>
2025-07-13 15:38:44 +0900

    add: project

---
+++ tripKit/tripKit/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  tripKit
+//
+//  Created by 이중엽 on 7/13/25.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tripKit/tripKit/tripKitApp.swift
@@ -0,0 +1,19 @@
+//
+//  tripKitApp.swift
+//  tripKit
+//
+//  Created by 이중엽 on 7/13/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct tripKitApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
